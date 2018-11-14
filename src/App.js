c15ac221251bdfa480b9f5a85bf5323bd857612c
Tylyps <j.chmielewski@o2.pl>
2018-11-14 23:46:48 +0100

    Connect first component to redux state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import AppRouter from './routers/AppRouter';
@@ -15,6 +16,10 @@ store.dispatch(addExpense({ description: 'Water bill', amount: 500 }));
 store.dispatch(addExpense({ description: 'Gas bill', amount: 200 }));
 store.dispatch(setTextFilter('bill'));
 
+setTimeout(() => {
+  store.dispatch(setTextFilter('rent'));
+}, 3000)
+
 let { expenses, filters } = store.getState();
 console.log(getVisibleExpenses(expenses, filters));
 store.dispatch(setTextFilter('water'));
@@ -22,4 +27,10 @@ expenses = store.getState().expenses;
 filters = store.getState().filters;
 console.log(getVisibleExpenses(store.getState().expenses, store.getState().filters));
 
-ReactDOM.render(<AppRouter />, appRoot);
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, appRoot);
