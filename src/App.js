b622e00c01067efc6e76c0a02d1ea8a9d9c1832f
Tylyps <j.chmielewski@o2.pl>
2018-11-14 18:05:03 +0100

    Organizing redux and use store

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-// import AppRouter from './routers/AppRouter';
-import PortfolioRouter from './routers/PortfolioRouter';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configuteStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 const appRoot = document.getElementById("app");
 
+const store = configureStore();
 
-ReactDOM.render(<PortfolioRouter />, appRoot);
+store.dispatch(addExpense({ description: 'Water bill', amount: 500 }));
+store.dispatch(addExpense({ description: 'Gas bill', amount: 200 }));
+store.dispatch(setTextFilter('bill'));
+
+let { expenses, filters } = store.getState();
+console.log(getVisibleExpenses(expenses, filters));
+store.dispatch(setTextFilter('water'));
+expenses = store.getState().expenses;
+filters = store.getState().filters;
+console.log(getVisibleExpenses(store.getState().expenses, store.getState().filters));
+
+ReactDOM.render(<AppRouter />, appRoot);
